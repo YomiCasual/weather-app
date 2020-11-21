d8164da9b5baf1e30ce85180d84355a7eb827ba7
YomiCasual <yomionisade@gmail.com>
2020-11-21 14:30:53 +0100

    second changes

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class App extends Component {
       })
 
       const searchItem = async(cityName) => {
-      const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9ba9717671a6bfb0092626f6325ab298`);
+      const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_CLIENT_ID}`);
 
      const response = await request.json()
      const data = response
